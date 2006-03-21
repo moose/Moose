6bf30233976676ca157c80174a7e73f0561dca4c
Stevan Little <stevan.little@iinteractive.com>
2006-03-21 02:02:27 +0000

    cleaning up

---
+++ lib/Moose/Meta/TypeCoercion.pm
@@ -0,0 +1,53 @@
+
+package Moose::Meta::TypeCoercion;
+
+use strict;
+use warnings;
+use metaclass;
+
+use Carp 'confess';
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Moose::Meta::TypeCoercion - The Moose Type Coercion metaobject
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=over 4
+
+=item B<meta>
+
+=back
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no 
+exception. If you find a bug please either email me, or add the bug
+to cpan-RT.
+
+=head1 AUTHOR
+
+Stevan Little E<lt>stevan@iinteractive.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2006 by Infinity Interactive, Inc.
+
+L<http://www.iinteractive.com>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
