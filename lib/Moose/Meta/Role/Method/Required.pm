faa0f9682fac28e3d7af39fa0db731b741ce2475
Stevan Little <stevan.little@iinteractive.com>
2007-08-07 02:31:23 +0000

    adding the Method::Required but not doing anything with it yet

---
+++ lib/Moose/Meta/Role/Method/Required.pm
@@ -0,0 +1,41 @@
+
+package Moose::Meta::Role::Method::Required;
+
+use strict;
+use warnings;
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Moose::Meta::Role::Method::Required - A Moose metaclass for required methods in Roles
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+All complex software has bugs lurking in it, and this module is no 
+exception. If you find a bug please either email me, or add the bug
+to cpan-RT.
+
+=head1 AUTHOR
+
+Stevan Little E<lt>stevan@iinteractive.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2006, 2007 by Infinity Interactive, Inc.
+
+L<http://www.iinteractive.com>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
