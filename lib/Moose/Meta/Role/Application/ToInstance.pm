9b28d20beb0387349f6a7d6906c30404231cd463
Stevan Little <stevan.little@iinteractive.com>
2008-01-16 16:57:18 +0000

    changelog stuff, mostly

--- lib/Moose/Meta/Role/Application/ToInstance.pm
+++ lib/Moose/Meta/Role/Application/ToInstance.pm
@@ -4,10 +4,8 @@ use strict;
 use warnings;
 use metaclass;
 
-use Carp            'confess';
-use Scalar::Util    'blessed';
-
-use Data::Dumper;
+use Carp         'confess';
+use Scalar::Util 'blessed';
 
 our $VERSION   = '0.01';
 our $AUTHORITY = 'cpan:STEVAN';
