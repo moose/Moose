fde3d43926269ad735f465ad5315ecf56b38cda7
Stevan Little <stevan.little@iinteractive.com>
2008-01-22 02:23:15 +0000

    this is not needed now

--- lib/Moose/Meta/Method/Constructor.pm
+++ lib/Moose/Meta/Method/Constructor.pm
@@ -93,7 +93,7 @@ sub intialize_body {
         # objects, rather than
         # Moose::Meta::Attribute. Class::MOP::Attribute attributes
         # have no type constraints.
-        my @type_constraints = map { $_->type_constraint } grep { $_->can('type_constraint') } @$attrs;
+        my @type_constraints = map { $_->type_constraint } @$attrs;
         my @type_constraint_bodies = map {
             $_ && $_->_compiled_type_constraint;
         } @type_constraints;
