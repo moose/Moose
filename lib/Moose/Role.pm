f4f808de5f4208523187e71c44eb4de34537a0b3
Yuval Kogman <nothingmuch@woobling.org>
2008-09-04 01:38:59 +0000

    Merge ../Moose-error into pluggable_errors

--- lib/Moose/Role.pm
+++ lib/Moose/Role.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 
 use Scalar::Util 'blessed';
-use Carp         'confess', 'croak';
+use Carp         'croak';
 
 use Data::OptList;
 use Sub::Exporter;
@@ -123,8 +123,7 @@ sub init_meta {
     my %args = @_;
 
     my $role = $args{for_class}
-        or confess
-        "Cannot call init_meta without specifying a for_class";
+        or Moose::throw_error("Cannot call init_meta without specifying a for_class");
 
     my $metaclass = $args{metaclass} || "Moose::Meta::Role";
 
@@ -136,7 +135,7 @@ sub init_meta {
     if ($role->can('meta')) {
         $meta = $role->meta();
         (blessed($meta) && $meta->isa('Moose::Meta::Role'))
-            || confess "You already have a &meta function, but it does not return a Moose::Meta::Role";
+            || Moose::throw_error("You already have a &meta function, but it does not return a Moose::Meta::Role");
     }
     else {
         $meta = $metaclass->initialize($role);
