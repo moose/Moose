b58714ddb3093af34999c048d6655a401ea73f64
Dave Rolsky <autarch@urth.org>
2008-09-14 02:56:06 +0000

    Add comment on my voodoo line which makes all my tests pass

--- lib/Moose/Meta/Class.pm
+++ lib/Moose/Meta/Class.pm
@@ -391,6 +391,11 @@ sub _reinitialize_with {
 
     bless $self, ref $new_self;
 
+    # FIXME? This seems to be necessary in some cases because of how
+    # Class::MOP::Class->construct_class_instance will weaken the
+    # metaclass store entry for an anonymous class. However, if that
+    # anonymous class is a metaclass's metaclass, we don't want it
+    # going out of scope. I'm not sure this is the right fix at all.
     Class::MOP::store_metaclass_by_name( $self->name, $self );
 }
 
