a532c4ac93ebdcbd09cd455dbb49caf8a7de1912
Dave Rolsky <autarch@urth.org>
2009-02-22 16:21:38 +0000

    make a dev version to go with the CMOP release

--- lib/Moose/Meta/Instance.pm
+++ lib/Moose/Meta/Instance.pm
@@ -4,7 +4,7 @@ package Moose::Meta::Instance;
 use strict;
 use warnings;
 
-our $VERSION   = '0.72';
+our $VERSION   = '0.71_01';
 $VERSION = eval $VERSION;
 our $AUTHORITY = 'cpan:STEVAN';
 
--- lib/Moose/Meta/Method/Delegation.pm
+++ lib/Moose/Meta/Method/Delegation.pm
@@ -7,7 +7,7 @@ use warnings;
 use Carp         'confess';
 use Scalar::Util 'blessed', 'weaken';
 
-our $VERSION   = '0.72';
+our $VERSION   = '0.71_01';
 $VERSION = eval $VERSION;
 our $AUTHORITY = 'cpan:STEVAN';
 
