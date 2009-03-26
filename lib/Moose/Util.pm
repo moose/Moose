b60c9fa0fe10866db2d969fb331554a257dd857c
Dave Rolsky <autarch@houseabsolute.urth.org>
2009-03-26 10:46:35 -0500

    trailing whitespace must die

--- lib/Moose/Util.pm
+++ lib/Moose/Util.pm
@@ -326,7 +326,7 @@ Copyright 2007-2009 by Infinity Interactive, Inc.
 L<http://www.iinteractive.com>
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. 
+it under the same terms as Perl itself.
 
 =cut
 
