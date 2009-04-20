aaf4b7357808399ed97d32026a42d6ee5ba85e78
Dave Rolsky <autarch@urth.org>
2009-04-20 10:23:57 -0500

    perltidy

--- lib/Moose/Meta/Class/Immutable/Trait.pm
+++ lib/Moose/Meta/Class/Immutable/Trait.pm
@@ -9,6 +9,8 @@ use base 'Class::MOP::Class::Immutable::Trait';
 
 sub add_role { shift->_immutable_cannot_call }
 
-sub calculate_all_roles { @{ $_[0]{__immutable}{calculate_all_roles} ||= [ shift->next::method ] } }
+sub calculate_all_roles {
+    @{ $_[0]{__immutable}{calculate_all_roles} ||= [ shift->next::method ] };
+}
 
 1;
