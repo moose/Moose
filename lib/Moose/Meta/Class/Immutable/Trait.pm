2dc27d6fac7271582ed8bf8d0ff05bac3504262c
Dave Rolsky <autarch@urth.org>
2009-04-20 13:19:54 -0500

    Add some minimal docs

--- lib/Moose/Meta/Class/Immutable/Trait.pm
+++ lib/Moose/Meta/Class/Immutable/Trait.pm
@@ -18,3 +18,32 @@ sub calculate_all_roles {
 }
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Moose::Meta::Class::Immutable::Trait - Implements immutability for metaclass objects
+
+=head1 DESCRIPTION
+
+This class makes some Moose-specific metaclass methods immutable. This
+is deep guts.
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 by Infinity Interactive, Inc.
+
+L<http://www.iinteractive.com>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
