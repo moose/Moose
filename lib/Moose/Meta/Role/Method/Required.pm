d7a5a65919cf2c026859558d957bcfd655ec24cc
Shawn M Moore <sartak@gmail.com>
2009-05-30 15:06:07 -0400

    Documentation

--- lib/Moose/Meta/Role/Method/Required.pm
+++ lib/Moose/Meta/Role/Method/Required.pm
@@ -33,6 +33,34 @@ Moose::Meta::Role::Method::Required - A Moose metaclass for required methods in 
 
 =head1 DESCRIPTION
 
+=head1 INHERITANCE
+
+C<Moose::Meta::Role::Method::Required> is a subclass of L<Class::MOP::Object>.
+It is B<not> a subclass of C<Moose::Meta::Role::Method> since it does not
+provide an implementation of the method.
+
+=head1 METHODS
+
+=over 4
+
+=item B<< Moose::Meta::Role::Method::Required->new(%options) >>
+
+This creates a new type constraint based on the provided C<%options>:
+
+=over 8
+
+=item * name
+
+The method name. This is required.
+
+=back
+
+=item B<< $method->name >>
+
+Returns the required method's name, as provided to the constructor.
+
+=back
+
 =head1 BUGS
 
 All complex software has bugs lurking in it, and this module is no
