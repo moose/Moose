33f819e155571b2becb24b564622ca799893914c
Chris Prather <chris@prather.org>
2009-08-09 16:22:55 -0400

    update documentation for Array so I can run it past confound and doy before doing more work

--- lib/Moose/Meta/Attribute/Native/MethodProvider/Array.pm
+++ lib/Moose/Meta/Attribute/Native/MethodProvider/Array.pm
@@ -1,7 +1,7 @@
 package Moose::Meta::Attribute::Native::MethodProvider::Array;
 use Moose::Role;
 
-our $VERSION   = '0.87';
+our $VERSION = '0.87';
 $VERSION = eval $VERSION;
 our $AUTHORITY = 'cpan:STEVAN';
 
@@ -43,7 +43,7 @@ sub sort : method {
     return sub {
         my ( $instance, $predicate ) = @_;
         die "Argument must be a code reference"
-            if $predicate && ref $predicate ne 'CODE';
+          if $predicate && ref $predicate ne 'CODE';
 
         if ($predicate) {
             CORE::sort { $predicate->( $a, $b ) } @{ $reader->($instance) };
@@ -99,16 +99,16 @@ sub push : method {
         $attr->has_type_constraint
         && $attr->type_constraint->isa(
             'Moose::Meta::TypeConstraint::Parameterized')
-        ) {
-        my $container_type_constraint
-            = $attr->type_constraint->type_parameter;
+      )
+    {
+        my $container_type_constraint = $attr->type_constraint->type_parameter;
         return sub {
             my $instance = CORE::shift;
             $container_type_constraint->check($_)
-                || confess "Value "
-                . ( $_ || 'undef' )
-                . " did not pass container type constraint '$container_type_constraint'"
-                foreach @_;
+              || confess "Value "
+              . ( $_ || 'undef' )
+              . " did not pass container type constraint '$container_type_constraint'"
+              foreach @_;
             CORE::push @{ $reader->($instance) } => @_;
         };
     }
@@ -133,16 +133,16 @@ sub unshift : method {
         $attr->has_type_constraint
         && $attr->type_constraint->isa(
             'Moose::Meta::TypeConstraint::Parameterized')
-        ) {
-        my $container_type_constraint
-            = $attr->type_constraint->type_parameter;
+      )
+    {
+        my $container_type_constraint = $attr->type_constraint->type_parameter;
         return sub {
             my $instance = CORE::shift;
             $container_type_constraint->check($_)
-                || confess "Value "
-                . ( $_ || 'undef' )
-                . " did not pass container type constraint '$container_type_constraint'"
-                foreach @_;
+              || confess "Value "
+              . ( $_ || 'undef' )
+              . " did not pass container type constraint '$container_type_constraint'"
+              foreach @_;
             CORE::unshift @{ $reader->($instance) } => @_;
         };
     }
@@ -174,14 +174,14 @@ sub set : method {
         $attr->has_type_constraint
         && $attr->type_constraint->isa(
             'Moose::Meta::TypeConstraint::Parameterized')
-        ) {
-        my $container_type_constraint
-            = $attr->type_constraint->type_parameter;
+      )
+    {
+        my $container_type_constraint = $attr->type_constraint->type_parameter;
         return sub {
             ( $container_type_constraint->check( $_[2] ) )
-                || confess "Value "
-                . ( $_[2] || 'undef' )
-                . " did not pass container type constraint '$container_type_constraint'";
+              || confess "Value "
+              . ( $_[2] || 'undef' )
+              . " did not pass container type constraint '$container_type_constraint'";
             $reader->( $_[0] )->[ $_[1] ] = $_[2];
         };
     }
@@ -199,9 +199,9 @@ sub accessor : method {
         $attr->has_type_constraint
         && $attr->type_constraint->isa(
             'Moose::Meta::TypeConstraint::Parameterized')
-        ) {
-        my $container_type_constraint
-            = $attr->type_constraint->type_parameter;
+      )
+    {
+        my $container_type_constraint = $attr->type_constraint->type_parameter;
         return sub {
             my $self = shift;
 
@@ -210,9 +210,9 @@ sub accessor : method {
             }
             elsif ( @_ == 2 ) {    # writer
                 ( $container_type_constraint->check( $_[1] ) )
-                    || confess "Value "
-                    . ( $_[1] || 'undef' )
-                    . " did not pass container type constraint '$container_type_constraint'";
+                  || confess "Value "
+                  . ( $_[1] || 'undef' )
+                  . " did not pass container type constraint '$container_type_constraint'";
                 $reader->($self)->[ $_[0] ] = $_[1];
             }
             else {
@@ -248,7 +248,7 @@ sub delete : method {
     my ( $attr, $reader, $writer ) = @_;
     return sub {
         CORE::splice @{ $reader->( $_[0] ) }, $_[1], 1;
-        }
+      }
 }
 
 sub insert : method {
@@ -257,14 +257,14 @@ sub insert : method {
         $attr->has_type_constraint
         && $attr->type_constraint->isa(
             'Moose::Meta::TypeConstraint::Parameterized')
-        ) {
-        my $container_type_constraint
-            = $attr->type_constraint->type_parameter;
+      )
+    {
+        my $container_type_constraint = $attr->type_constraint->type_parameter;
         return sub {
             ( $container_type_constraint->check( $_[2] ) )
-                || confess "Value "
-                . ( $_[2] || 'undef' )
-                . " did not pass container type constraint '$container_type_constraint'";
+              || confess "Value "
+              . ( $_[2] || 'undef' )
+              . " did not pass container type constraint '$container_type_constraint'";
             CORE::splice @{ $reader->( $_[0] ) }, $_[1], 0, $_[2];
         };
     }
@@ -281,16 +281,16 @@ sub splice : method {
         $attr->has_type_constraint
         && $attr->type_constraint->isa(
             'Moose::Meta::TypeConstraint::Parameterized')
-        ) {
-        my $container_type_constraint
-            = $attr->type_constraint->type_parameter;
+      )
+    {
+        my $container_type_constraint = $attr->type_constraint->type_parameter;
         return sub {
             my ( $self, $i, $j, @elems ) = @_;
             ( $container_type_constraint->check($_) )
-                || confess "Value "
-                . ( defined($_) ? $_ : 'undef' )
-                . " did not pass container type constraint '$container_type_constraint'"
-                for @elems;
+              || confess "Value "
+              . ( defined($_) ? $_ : 'undef' )
+              . " did not pass container type constraint '$container_type_constraint'"
+              for @elems;
             CORE::splice @{ $reader->($self) }, $i, $j, @elems;
         };
     }
@@ -308,12 +308,12 @@ sub sort_in_place : method {
         my ( $instance, $predicate ) = @_;
 
         die "Argument must be a code reference"
-            if $predicate && ref $predicate ne 'CODE';
+          if $predicate && ref $predicate ne 'CODE';
 
         my @sorted;
         if ($predicate) {
-            @sorted = CORE::sort { $predicate->( $a, $b ) }
-            @{ $reader->($instance) };
+            @sorted =
+              CORE::sort { $predicate->( $a, $b ) } @{ $reader->($instance) };
         }
         else {
             @sorted = CORE::sort @{ $reader->($instance) };
@@ -340,7 +340,7 @@ Moose::Meta::Attribute::Native::MethodProvider::Array
    use Moose::AttributeHelpers;
 
    has 'options' => (
-       metaclass  => 'Array',
+       traits     => ['Array'],
        is         => 'rw',
        isa        => 'ArrayRef[Str]',
        default    => sub { [] },
@@ -366,144 +366,13 @@ Moose::Meta::Attribute::Native::MethodProvider::Array
 =head1 DESCRIPTION
 
 This is a role which provides the method generators for
-L<Moose::Meta::Attribute::Trait::Native::Array>.
+L<Moose::Meta::Attribute::Trait::Native::Array>. Please check there for documentation on what methods are provided.
 
 =head1 METHODS
 
 =over 4
 
 =item B<meta>
-
-=back
-
-=head1 PROVIDED METHODS
-
-=over 4
-
-=item B<count>
-
-Returns the number of elements in the array.
-
-   $stuff = Stuff->new;
-   $stuff->options(["foo", "bar", "baz", "boo"]);
-
-   my $count = $stuff->count_options;
-   print "$count\n"; # prints 4
-
-=item B<empty>
-
-If the array is populated, returns true. Otherwise, returns false.
-
-   $stuff->do_i_have_options ? print "Good boy.\n" : die "No options!\n" ;
-
-=item B<find>
-
-This method accepts a subroutine reference as its argument. That sub
-will receive each element of the array in turn. If it returns true for
-an element, that element will be returned by the C<find> method.
-
-   my $found = $stuff->find_option( sub { $_[0] =~ /^b/ } );
-   print "$found\n"; # prints "bar"
-
-=item B<grep>
-
-This method accepts a subroutine reference as its argument. This
-method returns every element for which that subroutine reference
-returns a true value.
-
-   my @found = $stuff->filter_options( sub { $_[0] =~ /^b/ } );
-   print "@found\n"; # prints "bar baz boo"
-
-=item B<map>
-
-This method accepts a subroutine reference as its argument. The
-subroutine will be executed for each element of the array. It is
-expected to return a modified version of that element. The return
-value of the method is a list of the modified options.
-
-   my @mod_options = $stuff->map_options( sub { $_[0] . "-tag" } );
-   print "@mod_options\n"; # prints "foo-tag bar-tag baz-tag boo-tag"
-
-=item B<sort>
-
-Sorts and returns the elements of the array.
-
-You can provide an optional subroutine reference to sort with (as you
-can with the core C<sort> function). However, instead of using C<$a>
-and C<$b>, you will need to use C<$_[0]> and C<$_[1]> instead.
-
-   # ascending ASCIIbetical
-   my @sorted = $stuff->sort_options();
-
-   # Descending alphabetical order
-   my @sorted_options = $stuff->sort_options( sub { lc $_[1] cmp lc $_[0] } );
-   print "@sorted_options\n"; # prints "foo boo baz bar"
-
-=item B<elements>
-
-Returns all of the elements of the array
-
-   my @option = $stuff->all_options;
-   print "@options\n"; # prints "foo bar baz boo"
-
-=item B<join>
-
-Joins every element of the array using the separator given as argument.
-
-   my $joined = $stuff->join_options( ':' );
-   print "$joined\n"; # prints "foo:bar:baz:boo"
-
-=item B<get>
-
-Returns an element of the array by its index.
-
-   my $option = $stuff->get_option(1);
-   print "$option\n"; # prints "bar"
-
-=item B<first>
-
-Returns the first element of the array.
-
-   my $first = $stuff->first_option;
-   print "$first\n"; # prints "foo"
-
-=item B<last>
-
-Returns the last element of the array.
-
-   my $last = $stuff->last_option;
-   print "$last\n"; # prints "boo"
-
-=item B<pop>
-
-=item B<push>
-
-=item B<set>
-
-=item B<shift>
-
-=item B<unshift>
-
-=item B<clear>
-
-=item B<delete>
-
-=item B<insert>
-
-=item B<splice>
-
-=item B<sort_in_place>
-
-Sorts the array I<in place>, modifying the value of the attribute.
-
-You can provide an optional subroutine reference to sort with (as you
-can with the core C<sort> function). However, instead of using C<$a>
-and C<$b>, you will need to use C<$_[0]> and C<$_[1]> instead.
-
-=item B<accessor>
-
-If passed one argument, returns the value of the requested element.
-If passed two arguments, sets the value of the requested element.
 
 =back
 
--- lib/Moose/Meta/Attribute/Native/Trait/Array.pm
+++ lib/Moose/Meta/Attribute/Native/Trait/Array.pm
@@ -33,26 +33,171 @@ Moose::Meta::Attribute::Native::Trait::Array
 
 =head1 SYNOPSIS
 
-  package Stuff;
-  use Moose;
-  use Moose::AttributeHelpers;
+    package Stuff;
+    use Moose;
+    use Moose::AttributeHelpers;
 
-  has 'options' => (
-      metaclass => 'Array',
-      is        => 'ro',
-      isa       => 'ArrayRef[Int]',
-      default   => sub { [] },
-      handles   => {
-          add_options        => 'push',
-          remove_last_option => 'pop',
-      }
-  );
+    has 'options' => (
+       traits     => ['Array'],
+       is         => 'ro',
+       isa        => 'ArrayRef[Str]',
+       default    => sub { [] },
+       handles   => {
+           all_options       => 'elements',
+           map_options       => 'map',
+           filter_options    => 'grep',
+           find_option       => 'find',
+           first_option      => 'first',
+           last_option       => 'last',
+           get_option        => 'get',
+           join_options      => 'join',
+           count_options     => 'count',
+           do_i_have_options => 'empty',
+           sorted_options    => 'sort',
+       }
+    );
 
+    no Moose;
+    1;
+    
 =head1 DESCRIPTION
 
 This module provides an Array attribute which provides a number of
-array operations. See L<Moose::Meta::Attribute::Native::MethodProvider::Array>
-for more details.
+array operations. 
+
+=head1 PROVIDED METHODS
+
+These methods are implemented in
+L<Moose::Meta::Attribute::Native::MethodProvider::Array>.
+
+=over 4
+
+=item B<count>
+
+Returns the number of elements in the array.
+
+   $stuff = Stuff->new;
+   $stuff->options(["foo", "bar", "baz", "boo"]);
+
+   my $count = $stuff->count_options;
+   print "$count\n"; # prints 4
+
+=item B<empty>
+
+If the array is populated, returns true. Otherwise, returns false.
+
+   $stuff->do_i_have_options ? print "Good boy.\n" : die "No options!\n" ;
+
+=item B<find>
+
+This method accepts a subroutine reference as its argument. That sub
+will receive each element of the array in turn. If it returns true for
+an element, that element will be returned by the C<find> method.
+
+   my $found = $stuff->find_option( sub { $_[0] =~ /^b/ } );
+   print "$found\n"; # prints "bar"
+
+=item B<grep>
+
+This method accepts a subroutine reference as its argument. This
+method returns every element for which that subroutine reference
+returns a true value.
+
+   my @found = $stuff->filter_options( sub { $_[0] =~ /^b/ } );
+   print "@found\n"; # prints "bar baz boo"
+
+=item B<map>
+
+This method accepts a subroutine reference as its argument. The
+subroutine will be executed for each element of the array. It is
+expected to return a modified version of that element. The return
+value of the method is a list of the modified options.
+
+   my @mod_options = $stuff->map_options( sub { $_[0] . "-tag" } );
+   print "@mod_options\n"; # prints "foo-tag bar-tag baz-tag boo-tag"
+
+=item B<sort>
+
+Sorts and returns the elements of the array.
+
+You can provide an optional subroutine reference to sort with (as you
+can with the core C<sort> function). However, instead of using C<$a>
+and C<$b>, you will need to use C<$_[0]> and C<$_[1]> instead.
+
+   # ascending ASCIIbetical
+   my @sorted = $stuff->sort_options();
+
+   # Descending alphabetical order
+   my @sorted_options = $stuff->sort_options( sub { lc $_[1] cmp lc $_[0] } );
+   print "@sorted_options\n"; # prints "foo boo baz bar"
+
+=item B<elements>
+
+Returns all of the elements of the array
+
+   my @option = $stuff->all_options;
+   print "@options\n"; # prints "foo bar baz boo"
+
+=item B<join>
+
+Joins every element of the array using the separator given as argument.
+
+   my $joined = $stuff->join_options( ':' );
+   print "$joined\n"; # prints "foo:bar:baz:boo"
+
+=item B<get>
+
+Returns an element of the array by its index.
+
+   my $option = $stuff->get_option(1);
+   print "$option\n"; # prints "bar"
+
+=item B<first>
+
+Returns the first element of the array.
+
+   my $first = $stuff->first_option;
+   print "$first\n"; # prints "foo"
+
+=item B<last>
+
+Returns the last element of the array.
+
+   my $last = $stuff->last_option;
+   print "$last\n"; # prints "boo"
+
+=item B<pop>
+
+=item B<push>
+
+=item B<set>
+
+=item B<shift>
+
+=item B<unshift>
+
+=item B<clear>
+
+=item B<delete>
+
+=item B<insert>
+
+=item B<splice>
+
+=item B<sort_in_place>
+
+Sorts the array I<in place>, modifying the value of the attribute.
+
+You can provide an optional subroutine reference to sort with (as you
+can with the core C<sort> function). However, instead of using C<$a>
+and C<$b>, you will need to use C<$_[0]> and C<$_[1]> instead.
+
+=item B<accessor>
+
+If passed one argument, returns the value of the requested element.
+If passed two arguments, sets the value of the requested element.
+
+=back
 
 =head1 METHODS
 
