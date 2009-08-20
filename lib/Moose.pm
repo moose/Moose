0bb2cc8a6a2e4b967b5d8efbcf1ab27f5d95e990
Cory Watson <github@onemogin.com>
2009-08-20 17:27:37 -0500

    Fix builder's recipe link.

--- lib/Moose.pm
+++ lib/Moose.pm
@@ -607,7 +607,7 @@ example.
 The value of this key is the name of the method that will be called to
 obtain the value used to initialize the attribute. See the L<builder
 option docs in Class::MOP::Attribute|Class::MOP::Attribute/builder>
- and/or L<Moose::Cookbook::Basics::Recipe9> for more information.
+ and/or L<Moose::Cookbook::Basics::Recipe8> for more information.
 
 =item I<default> => SCALAR | CODE
 
