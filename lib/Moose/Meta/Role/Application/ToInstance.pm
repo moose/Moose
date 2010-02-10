a687ab448365b1a059b4d0b96e7e79419b116840
Rafael Kitover <rkitover@cpan.org>
2010-02-10 12:09:28 -0500

    use "roles" when creating anon class for applying a role to instance

--- lib/Moose/Meta/Role/Application/ToInstance.pm
+++ lib/Moose/Meta/Role/Application/ToInstance.pm
@@ -29,10 +29,10 @@ sub apply {
         unless $obj_meta->isa('Moose::Meta::Class');
 
     my $class = $obj_meta->create_anon_class(
-        superclasses => [ blessed($object) ], cache => 1,
+        superclasses => [ blessed($object) ],
+        roles => [ $role ],
+        cache => 1,
     );
-
-    $self->SUPER::apply( $role, $class );
 
     $class->rebless_instance( $object, %{ $self->rebless_params } );
 }
