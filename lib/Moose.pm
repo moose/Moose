45902e412706a3ce9e0d52b4a0bbbfdd51c2c2ae
Florian Ragwitz <rafl@debian.org>
2010-05-28 01:09:29 +0200

    Line wrapping

--- lib/Moose.pm
+++ lib/Moose.pm
@@ -354,7 +354,10 @@ Much of the Moose documentation has been translated into other languages.
 
 =item Japanese
 
-Japanese docs can be found at L<http://perldoc.perlassociation.org/pod/Moose-Doc-JA/index.html>. The source POD files can be found in GitHub: L<http://github.com/jpa/Moose-Doc-JA>
+Japanese docs can be found at
+L<http://perldoc.perlassociation.org/pod/Moose-Doc-JA/index.html>. The
+source POD files can be found in GitHub:
+L<http://github.com/jpa/Moose-Doc-JA>
 
 =back
 
