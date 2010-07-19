217312a53b843270d91c7bf54f953979b534b97e
Dave Rolsky <autarch@urth.org>
2010-07-19 12:05:57 -0500

    Add coerce without coercion to list of deprecated features

--- lib/Moose/Deprecated.pm
+++ lib/Moose/Deprecated.pm
@@ -8,6 +8,7 @@ $VERSION = eval $VERSION;
 our $AUTHORITY = 'cpan:STEVAN';
 
 use Package::DeprecationManager -deprecations => {
+    'coerce without coercion'     => '1.08',
     'pre-0.94 MetaRole API'       => '0.93',
     'Role type'                   => '0.84',
     'subtype without sugar'       => '0.72',
