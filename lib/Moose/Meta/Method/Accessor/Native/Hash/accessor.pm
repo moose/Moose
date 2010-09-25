d352fb4c37cdb0d0235ac11b662dc17f74be1802
Dave Rolsky <autarch@urth.org>
2010-09-25 14:52:31 -0500

    Use _writer_core
    Get _potential_value from set

--- lib/Moose/Meta/Method/Accessor/Native/Hash/accessor.pm
+++ lib/Moose/Meta/Method/Accessor/Native/Hash/accessor.pm
@@ -44,32 +44,8 @@ sub _generate_method {
     # set
     $code .= "\n" . '} else {';
 
-    $code .= "\n" . $self->_inline_check_argument_count;
-    $code .= "\n" . $self->_inline_process_arguments( $inv, $slot_access );
-    $code
-        .= "\n"
-        . $self
-        ->Moose::Meta::Method::Accessor::Native::Hash::set::_inline_check_arguments;
+    $code .= "\n" . $self->_writer_core( $inv, $slot_access );
 
-    my $potential_value = $self->_potential_value($slot_access);
-
-    $code .= "\n"
-        . $self->_inline_tc_code(
-        $potential_value,
-        $slot_access,
-        );
-
-    $code .= "\n" . $self->_inline_get_old_value_for_trigger($inv);
-    $code .= "\n" . $self->_inline_capture_return_value($slot_access);
-
-    $code .= "\n"
-        . $self->_inline_set_new_value(
-        $inv,
-        $potential_value,
-        $slot_access,
-        );
-
-    $code .= "\n" . $self->_inline_trigger( $inv, $slot_access, '@old' );
     $code .= "\n" . $self->_inline_post_body(@_);
 
     $code .= "\n}";
@@ -83,12 +59,6 @@ sub _minimum_arguments {2}
 sub _maximum_arguments {2}
 
 sub _adds_members {1}
-
-sub _potential_value {
-    my ( $self, $slot_access ) = @_;
-
-    return "%{ $slot_access, @_ }";
-}
 
 sub _new_members {'$_[1]'}
 
