9fa449cd4a3fdd9e62d0b06e1df9c3e72b6bebe5
Dave Rolsky <autarch@urth.org>
2010-09-25 14:52:07 -0500

    Just use _writer_core for accessor
    Get _potential_value from set class

--- lib/Moose/Meta/Method/Accessor/Native/Array/accessor.pm
+++ lib/Moose/Meta/Method/Accessor/Native/Array/accessor.pm
@@ -44,33 +44,8 @@ sub _generate_method {
     # set
     $code .= "\n" . '} else {';
 
-    $code .= "\n" . $self->_inline_check_argument_count;
-    $code .= "\n" . $self->_inline_process_arguments( $inv, $slot_access );
-    $code
-        .= "\n"
-        . $self
-        ->Moose::Meta::Method::Accessor::Native::Array::set::_inline_check_arguments;
+    $code .= "\n" . $self->_writer_core( $inv, $slot_access );
 
-    my $potential_value = $self->_potential_value($slot_access);
-
-    $code .= "\n" . $self->_inline_copy_value( \$potential_value );
-    $code .= "\n"
-        . $self->_inline_tc_code(
-        $potential_value,
-        $slot_access,
-        );
-
-    $code .= "\n" . $self->_inline_get_old_value_for_trigger($inv);
-    $code .= "\n" . $self->_inline_capture_return_value($slot_access);
-
-    $code .= "\n"
-        . $self->_inline_set_new_value(
-        $inv,
-        $potential_value,
-        $slot_access,
-        );
-
-    $code .= "\n" . $self->_inline_trigger( $inv, $slot_access, '@old' );
     $code .= "\n" . $self->_inline_post_body(@_);
 
     $code .= "\n}";
@@ -84,13 +59,6 @@ sub _minimum_arguments {2}
 sub _maximum_arguments {2}
 
 sub _adds_members {1}
-
-sub _potential_value {
-    my ( $self, $slot_access ) = @_;
-
-    return
-        "( do { my \@potential = \@{ $slot_access }; \$potential[ \$_[0] ] = \$_[1]; \@potential } )";
-}
 
 sub _new_members {'$_[1]'}
 
