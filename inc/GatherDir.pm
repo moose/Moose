a83cf79cd1d1e3c0ff174bd57f52d5d0261a2547
Jesse Luehrs <doy@tozt.net>
2011-01-01 15:36:59 -0600

    pruning t/002 isn't correct, need to prevent it from being added at all

---
+++ inc/GatherDir.pm
@@ -0,0 +1,16 @@
+package inc::GatherDir;
+
+use Moose;
+
+extends 'Dist::Zilla::Plugin::GatherDir';
+
+around _file_from_filename => sub {
+    my $orig = shift;
+    my $self = shift;
+    my $file = $self->$orig(@_);
+    return $file if $file->name =~ m+^t/002_recipes/basics_recipe10\.t+;
+    return ()    if $file->name =~ m+^t/002_recipes+;
+    return $file;
+};
+
+1;
