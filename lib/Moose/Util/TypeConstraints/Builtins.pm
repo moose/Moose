b47527bf39db1d3daa087bff51a99a9be5af739c
Dave Rolsky <autarch@urth.org>
2011-06-05 10:13:04 -0500

    Just fetch Value type once rather than each time inlining is done for Num and Int

--- lib/Moose/Util/TypeConstraints/Builtins.pm
+++ lib/Moose/Util/TypeConstraints/Builtins.pm
@@ -78,12 +78,13 @@ sub define_builtins {
             . ')'
         };
 
+    my $value_type = Moose::Util::TypeConstraints::find_type_constraint('Value');
     subtype 'Num'
         => as 'Str'
         => where { Scalar::Util::looks_like_number($_) }
         => inline_as {
             # the long Str tests are redundant here
-            Moose::Util::TypeConstraints::find_type_constraint('Value')->_inline_check($_[1])
+            $value_type->_inline_check($_[1])
             . ' && Scalar::Util::looks_like_number(' . $_[1] . ')'
         };
 
@@ -91,7 +92,7 @@ sub define_builtins {
         => as 'Num'
         => where { (my $val = $_) =~ /\A-?[0-9]+\z/ }
         => inline_as {
-            Moose::Util::TypeConstraints::find_type_constraint('Value')->_inline_check($_[1])
+            $value_type->_inline_check($_[1])
             . ' && (my $val = ' . $_[1] . ') =~ /\A-?[0-9]+\z/'
         };
 
