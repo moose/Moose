e5a728d90e9d494fc8e6cb93f504e0d851d00cb4
Dave Rolsky <autarch@urth.org>
2012-02-11 11:57:09 -0600

    Rename Basics::Recipe5 to Basics::HTTP_SubtypesAndCoercion

--- lib/Moose.pm
+++ lib/Moose.pm
@@ -459,7 +459,7 @@ for information on how to define a new type, and how to retrieve type meta-data)
 This will attempt to use coercion with the supplied type constraint to change
 the value passed into any accessors or constructors. You B<must> supply a type
 constraint, and that type constraint B<must> define a coercion. See
-L<Moose::Cookbook::Basics::Recipe5> for an example.
+L<Moose::Cookbook::Basics::HTTP_SubtypesAndCoercion> for an example.
 
 =item I<does =E<gt> $role_name>
 
