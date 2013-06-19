469a37ee70890894dc8c540892c46e757a60a8aa
Upasana Shukla <me@upasana.me>
2013-06-20 02:57:51 +0530

    Removed tabs

--- lib/Moose/Exception/ExtendsMissingArgs.pm
+++ lib/Moose/Exception/ExtendsMissingArgs.pm
@@ -7,14 +7,14 @@ has 'trace' => (
     is => 'rw',
     isa => 'Devel::StackTrace',
     default => sub { Devel::StackTrace->new(
-	    message => "Must derive at least one class",
-	    indent => 1, );
+        message => "Must derive at least one class",
+        indent => 1, );
     },
 );
 
 use overload
     '""' => sub {
-	my $self = shift;
-	return $self->trace->as_string;
+        my $self = shift;
+        return $self->trace->as_string;
 };
 1;
