a7476a4280475fb232397232e749222a01d4b656
Upasana Shukla <me@upasana.me>
2013-06-24 03:25:15 +0530

    Written an Exception class for invalid characters for a type name

---
+++ lib/Moose/Exception/InvalidNameForType.pm
@@ -0,0 +1,18 @@
+package Moose::Exception::InvalidNameForType;
+
+use Moose;
+use Moose::Exception;
+
+extends 'Moose::Exception';
+
+has 'name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    $self->name." contains invalid characters for a type name. Names can contain alphanumeric character, ':', and '.'";     
+}
+1;
