2198a908564b66d2027a5b7c62dba9653634e65a
Upasana Shukla <me@upasana.me>
2013-06-24 20:40:27 +0530

    Wrote sub throw_exception

--- lib/Moose.pm
+++ lib/Moose.pm
@@ -40,6 +40,13 @@ use Moose::Util 'throw_exception';
 
 use Moose::Meta::Attribute::Native;
 
+sub throw_exception {
+    my ($self, @args_to_exception) = @_;
+    my $class = "Moose::Exception::$self";
+    load_class( $class );
+    die $class->new( @args_to_exception );
+}
+
 sub throw_error {
     # FIXME This
     shift;
@@ -51,8 +58,7 @@ sub extends {
 
     unless ( @_ )
     {
-        require Moose::Exception::ExtendsMissingArgs;
-        die Moose::Exception::ExtendsMissingArgs->new;
+        throw_exception( ExtendsMissingArgs => {} );
     }
     # this checks the metaclass to make sure
     # it is correct, sometimes it can get out
