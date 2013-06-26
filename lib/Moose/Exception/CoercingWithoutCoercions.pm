80b096b2a091201f1276fbd68e2cf4f9e18318d3
Upasana Shukla <me@upasana.me>
2013-06-27 03:26:53 +0530

    Added exception class for coercing without type coercions

--- lib/Moose/Exception/CoercingWithoutCoercions.pm
+++ lib/Moose/Exception/CoercingWithoutCoercions.pm
@@ -5,12 +5,6 @@ use Moose::Exception;
 
 extends 'Moose::Exception';
 
-has 'type' => (
-    is       => 'ro',
-    isa      => "Moose::Meta::TypeConstraint",
-    required => 1,
-);
-
 sub _build_message {
     my $self = shift;
     "Cannot coerce ".$self->type." without a type coercion";
