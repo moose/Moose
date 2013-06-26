cc83936f9fd4a749714d7cee036572c099ede7f9
Upasana Shukla <me@upasana.me>
2013-06-26 23:24:26 +0530

    added throw_exception for cannot coerce without a type coercion

--- lib/Moose/Meta/TypeConstraint.pm
+++ lib/Moose/Meta/TypeConstraint.pm
@@ -20,6 +20,8 @@ use base 'Class::MOP::Object';
 
 use Moose::Util 'throw_exception';
 
+use Moose::Util 'throw_exception';
+
 __PACKAGE__->meta->add_attribute('name'       => (
     reader => 'name',
     Class::MOP::_definition_context(),
