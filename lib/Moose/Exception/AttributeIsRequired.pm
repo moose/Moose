7f08cd30c09a192e9b0864ae71e96c1aa81277b6
Upasana Shukla <me@upasana.me>
2013-06-28 08:16:41 +0530

    Wrote exception class for AttributeIsRequired

---
+++ lib/Moose/Exception/AttributeIsRequired.pm
@@ -0,0 +1,31 @@
+package Moose::Exception::AttributeIsRequired;
+
+use Moose;
+use Moose::Exception;
+
+extends 'Moose::Exception';
+
+has 'attribute' => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::Attribute',
+    required => 1,
+);
+
+has 'instance' => (
+    is       => 'ro',
+    isa      => 'Object',
+    required => 1,
+);
+    
+has 'params' => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "Attribute (".$self->attribute->name.") is required";
+}
+
+1;
