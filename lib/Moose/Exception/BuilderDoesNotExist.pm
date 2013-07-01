a71d5518e17ebe40bf7439f31f4939b723c7676b
Upasana Shukla <me@upasana.me>
2013-07-01 05:43:52 +0530

    used Role Attribute and Instance

--- lib/Moose/Exception/BuilderDoesNotExist.pm
+++ lib/Moose/Exception/BuilderDoesNotExist.pm
@@ -2,18 +2,7 @@ package Moose::Exception::BuilderDoesNotExist;
 
 use Moose;
 extends 'Moose::Exception';
-
-has 'instance' => (
-    is       => 'ro',
-    isa      => 'Object',
-    required => 1,
-);
-
-has 'attribute' => (
-    is       => 'ro',
-    isa      => 'Moose::Meta::Attribute',
-    required => 1,
-);
+with 'Moose::Exception::Role::Attribute', 'Moose::Exception::Role::Instance';
 
 sub _build_message {
     my $self = shift;
