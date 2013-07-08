851fc12427e004d191bcd07c5b8fce948ae7297c
Upasana Shukla <me@upasana.me>
2013-07-08 08:03:18 +0530

    Wrote CannotOverrideLocalMethodIsPresent

---
+++ lib/Moose/Exception/CannotOverrideLocalMethodIsPresent.pm
@@ -0,0 +1,11 @@
+package Moose::Exception::CannotOverrideLocalMethodIsPresent;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class', 'Moose::Exception::Role::Method';
+
+sub _build_message {
+    "Cannot add an override method if a local method is already present";
+}
+
+1;
