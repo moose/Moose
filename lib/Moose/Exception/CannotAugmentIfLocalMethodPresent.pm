2038383e9cd41e8efd902409ba762e256f8e0663
Upasana Shukla <me@upasana.me>
2013-07-08 07:45:47 +0530

    Used roles for class & method

--- lib/Moose/Exception/CannotAugmentIfLocalMethodPresent.pm
+++ lib/Moose/Exception/CannotAugmentIfLocalMethodPresent.pm
@@ -2,18 +2,7 @@ package Moose::Exception::CannotAugmentIfLocalMethodPresent;
 
 use Moose;
 extends 'Moose::Exception';
-
-has 'class' => (
-    is       => 'ro',
-    isa      => 'Moose::Meta::Class',
-    required => 1,
-);
-
-has 'method' => (
-    is       => 'ro',
-    isa      => 'Moose::Meta::Method',
-    required => 1,
-);
+with 'Moose::Exception::Role::Class', 'Moose::Exception::Role::Method';
 
 sub _build_message {
     "Cannot add an augment method if a local method is already present";
