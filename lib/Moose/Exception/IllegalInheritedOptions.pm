4393b6be0cc8f7126c44b75d0c1cc656fb0f9a23
Upasana Shukla <me@upasana.me>
2013-07-10 02:38:44 +0530

    Wrote IllegalInheritedOptions

---
+++ lib/Moose/Exception/IllegalInheritedOptions.pm
@@ -0,0 +1,26 @@
+package Moose::Exception::IllegalInheritedOptions;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'illegal_options' => (
+    is       => 'ro',
+    traits   => ['Array'],
+    handles  => { 
+	_join_options => 'join',
+    },
+    required => 1,
+);
+
+has 'params' => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "Illegal inherited options => (".$self->_join_options(', ').")";
+}
+
+1;
