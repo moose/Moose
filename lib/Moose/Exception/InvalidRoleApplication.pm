1dad4581905c0e314349b2cc9d33455b6acaae62
Upasana Shukla <me@upasana.me>
2013-07-09 16:57:23 +0530

    Wrote InvalidRoleApplication, NoAttributeFoundInSuperClass & RolesInCreateTakesAnArrayRef

---
+++ lib/Moose/Exception/InvalidRoleApplication.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::InvalidRoleApplication;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class';
+
+has 'application' => (
+    is       => 'ro',
+    isa      => "Any",
+    required => 1,
+);
+
+sub _build_message {
+    "Role applications must be instances of Moose::Meta::Role::Application::ToClass";
+}
+
+1;
---
+++ lib/Moose/Exception/NoAttributeFoundInSuperClass.pm
@@ -0,0 +1,12 @@
+package Moose::Exception::NoAttributeFoundInSuperClass;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class', 'Moose::Exception::Role::InvalidAttributeOptions';
+
+sub _build_message {
+    my $self = shift;
+    "Could not find an attribute by the name of '".$self->attribute_name."' to inherit from in ".$self->class->name;
+}
+
+1;
