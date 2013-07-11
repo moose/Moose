4d846fd1581e76ebc3f4e5727e73d6a044399ad1
Upasana Shukla <me@upasana.me>
2013-07-11 20:51:25 +0530

    Wrote DelegationToAClassWhichIsNotLoaded

---
+++ lib/Moose/Exception/DelegationToAClassWhichIsNotLoaded.pm
@@ -0,0 +1,18 @@
+package Moose::Exception::DelegationToAClassWhichIsNotLoaded;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Attribute';
+    
+has 'class_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "The ".$self->attribute->name." attribute is trying to delegate to a class which has not been loaded - ".$self->class_name;
+}
+
+1;
