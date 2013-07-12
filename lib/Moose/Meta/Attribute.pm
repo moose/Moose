efb740021622d107a4fb07e114a5e3153b5b84dc
Upasana Shukla <me@upasana.me>
2013-07-12 19:03:13 +0530

    replaced throw_error with throw_exception

--- lib/Moose/Meta/Attribute.pm
+++ lib/Moose/Meta/Attribute.pm
@@ -1239,7 +1239,7 @@ sub _find_delegate_metaclass {
         return Class::MOP::class_of($role);
     }
     else {
-        $self->throw_error("Cannot find delegate metaclass for attribute " . $self->name);
+	throw_exception( CannotFindDelegateMetaclass => attribute => $self );
     }
 }
 
