43d8a5693329f8b0b2f8281ebb9b64ea4f6da70a
Upasana Shukla <me@upasana.me>
2013-07-14 22:04:23 +0530

    Wrote ClassNamesDoNotMatch

---
+++ lib/Moose/Exception/ClassNamesDoNotMatch.pm
@@ -0,0 +1,23 @@
+package Moose::Exception::ClassNamesDoNotMatch;
+
+use Moose;
+extends 'Moose::Exception';
+
+has class_name => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has class => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::Class',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "class_name (".$self-> class_name.") does not match class->name (".$self->class->name.")";
+}
+
+1;
