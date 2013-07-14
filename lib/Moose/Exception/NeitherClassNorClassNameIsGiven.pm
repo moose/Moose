1dd1991428c9aef7515c1bd27059dce55ca330cb
Upasana Shukla <me@upasana.me>
2013-07-14 22:03:56 +0530

    removed whitespace

--- lib/Moose/Exception/NeitherClassNorClassNameIsGiven.pm
+++ lib/Moose/Exception/NeitherClassNorClassNameIsGiven.pm
@@ -2,7 +2,7 @@ package Moose::Exception::NeitherClassNorClassNameIsGiven;
 
 use Moose;
 extends 'Moose::Exception';
-    
+
 sub _build_message {
     "You need to give class or class_name or both";
 }
