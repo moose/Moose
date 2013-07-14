da8fb0090ea1bf0ed7166a306f4fe425ffc9904d
Upasana Shukla <me@upasana.me>
2013-07-14 22:01:22 +0530

    Modified Moose::Exception::Role::Class to throw NeitherClassNorClassNameIsGiven & ClassNamesDoNotMatch

--- lib/Moose/Exception/Role/Class.pm
+++ lib/Moose/Exception/Role/Class.pm
@@ -1,11 +1,49 @@
 package Moose::Exception::Role::Class;
 
+use Moose::Util 'throw_exception';
 use Moose::Role;
 
 has 'class' => (
-    is       => 'ro',
-    isa      => 'Moose::Meta::Class',
-    required => 1,
+    is        => 'rw',
+    isa       => 'Moose::Meta::Class',
+    lazy      => 1,
+    builder   => '_build_class',
+    predicate => 'is_class_set',
 );
+
+has 'class_name' => (
+    is        => 'ro',
+    isa       => 'Str',
+    lazy      => 1,
+    builder   => '_build_class_name',
+    predicate => 'is_class_name_set',
+);
+
+sub _build_class {
+    my $self = $_[0];
+    Class::MOP::class_of( $self->class_name );
+}
+
+sub _build_class_name {
+    my $self = $_[0];
+    $self->class->name;
+}
+
+after "BUILD" => sub {
+    my $self = $_[0];
+    if( !( $self->is_class_name_set) && !( $self->is_class_set) )
+    {
+	throw_exception("NeitherClassNorClassNameIsGiven");
+    }
+
+    if( $self->is_class_name_set &&
+	$self->is_class_set &&
+	( $self->class->name ne $self->class_name ) )
+    {
+        throw_exception( ClassNamesDoNotMatch => class_name => $self->class_name,
+                                                 class      => $self->class,
+                       );
+    }
+};
 
 1;
