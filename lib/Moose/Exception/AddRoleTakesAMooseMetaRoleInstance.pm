6e85b475c09b7d49a1a3665e886e36168ad46d27
Upasana Shukla <me@upasana.me>
2013-07-16 04:51:42 +0530

    added attribute class & role_to_be_added to AddRoleTakesAMooseMetaRoleInstance

--- lib/Moose/Exception/AddRoleTakesAMooseMetaRoleInstance.pm
+++ lib/Moose/Exception/AddRoleTakesAMooseMetaRoleInstance.pm
@@ -2,6 +2,13 @@ package Moose::Exception::AddRoleTakesAMooseMetaRoleInstance;
 
 use Moose;
 extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class';
+
+has 'role_to_be_added' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1,
+);
 
 sub _build_message {
     "Roles must be instances of Moose::Meta::Role";
