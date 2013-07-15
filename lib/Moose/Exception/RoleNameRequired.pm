6161049d57aac5de46d74cf83e72befe81b1162f
Upasana Shukla <me@upasana.me>
2013-07-16 04:54:36 +0530

    Added class attribute to RoleNameRequired

--- lib/Moose/Exception/RoleNameRequired.pm
+++ lib/Moose/Exception/RoleNameRequired.pm
@@ -2,6 +2,7 @@ package Moose::Exception::RoleNameRequired;
 
 use Moose;
 extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class';
 
 sub _build_message {
     "You must supply a role name to look for";
