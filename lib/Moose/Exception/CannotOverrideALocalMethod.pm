ccb24b8dd01f60a1a3f4b89efbac4e23fc787810
Upasana Shukla <me@upasana.me>
2013-07-16 03:45:02 +0530

    Wrote exception class for CannotOverrideALocalMethod

---
+++ lib/Moose/Exception/CannotOverrideALocalMethod.pm
@@ -0,0 +1,18 @@
+package Moose::Exception::CannotOverrideALocalMethod;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Role';
+
+has 'method_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "Cannot add an override of method '".$self->method_name."' because there is a local version of '".$self->method_name."'";
+}
+
+1;
