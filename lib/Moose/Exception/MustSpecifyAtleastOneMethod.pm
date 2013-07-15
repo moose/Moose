d5edd394e031359df79c3ec106c521eb149e56ba
Upasana Shukla <me@upasana.me>
2013-07-15 15:56:44 +0530

    Wrote exception MustSpecifyAtLeastOneMethod

---
+++ lib/Moose/Exception/MustSpecifyAtleastOneMethod.pm
@@ -0,0 +1,11 @@
+package Moose::Exception::MustSpecifyAtleastOneMethod;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Role';
+    
+sub _build_message {
+    "Must specify at least one method";
+}
+
+1;
