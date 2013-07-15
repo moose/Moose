df87f132e0ddbb150b2bc322ee3db102c395dd57
Upasana Shukla <me@upasana.me>
2013-07-15 15:24:32 +0530

    Wrote RolesDoNotSupportExtends

---
+++ lib/Moose/Exception/RolesDoNotSupportExtends.pm
@@ -0,0 +1,10 @@
+package Moose::Exception::RolesDoNotSupportExtends;
+
+use Moose;
+extends 'Moose::Exception';
+    
+sub _build_message {
+    "Roles do not support 'extends' (you can use 'with' to specialize a role)";
+}
+
+1;
