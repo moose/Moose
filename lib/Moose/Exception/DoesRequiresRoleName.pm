7474271658b284ee709026a113b00672e6be165b
Upasana Shukla <me@upasana.me>
2013-07-15 22:35:43 +0530

    Added object to DoesRequiresRoleName

--- lib/Moose/Exception/DoesRequiresRoleName.pm
+++ lib/Moose/Exception/DoesRequiresRoleName.pm
@@ -3,6 +3,12 @@ package Moose::Exception::DoesRequiresRoleName;
 use Moose;
 extends 'Moose::Exception';
 
+has 'object' => (
+    is       => 'ro',
+    isa      => 'Moose::Object',
+    required => 1,
+);
+
 sub _build_message {
     "You must supply a role name to does()";
 }
