8c14d8e5a08335c9ea64604a503b3fe5c10749fe
Upasana Shukla <me@upasana.me>
2013-07-18 19:10:55 +0530

    wrote AddParameterizableTypeTakesParameterizableType,CannotFindType & TypeConstraintIsAlreadyCreated

---
+++ lib/Moose/Exception/AddParameterizableTypeTakesParameterizableType.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::AddParameterizableTypeTakesParameterizableType;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'type_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "Type must be a Moose::Meta::TypeConstraint::Parameterizable not ".$self->type_name;
+}
+
+1;
