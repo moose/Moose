d104b11a3b549e37c1fcd06e382ac8ae495f9bfb
Upasana Shukla <me@upasana.me>
2013-07-20 01:04:10 +0530

    consumed Moose::Exception::Role::Class

--- lib/Moose/Exception/MethodNameNotGiven.pm
+++ lib/Moose/Exception/MethodNameNotGiven.pm
@@ -2,12 +2,7 @@ package Moose::Exception::MethodNameNotGiven;
 
 use Moose;
 extends 'Moose::Exception';
-
-has 'class' => (
-    is       => 'ro',
-    isa      => 'Class::MOP::Class',
-    required => 1
-);
+with 'Moose::Exception::Role::Class';
 
 sub _build_message {
     "You must define a method name to find";
