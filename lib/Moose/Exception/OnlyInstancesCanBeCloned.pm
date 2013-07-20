1bf429a18e4ab9e284993618bca4e530e9a72425
Upasana Shukla <me@upasana.me>
2013-07-21 04:19:46 +0530

    Wrote OnlyInstancesCanBeCloned

---
+++ lib/Moose/Exception/OnlyInstancesCanBeCloned.pm
@@ -0,0 +1,18 @@
+package Moose::Exception::OnlyInstancesCanBeCloned;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class', 'Moose::Exception::Role::ParamsHash';
+
+has 'instance' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "You can only clone instances, (".$self->instance.") is not a blessed instance";
+}
+
+1;
