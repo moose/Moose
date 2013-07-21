e5e7aa559ec3416c3d95e49b1da40b80651a3f7f
Upasana Shukla <me@upasana.me>
2013-07-21 22:38:16 +0530

    modified exception classes to use ParamsHash Role

--- lib/Moose/Exception/AttributeIsRequired.pm
+++ lib/Moose/Exception/AttributeIsRequired.pm
@@ -3,10 +3,10 @@ package Moose::Exception::AttributeIsRequired;
 use Moose;
 extends 'Moose::Exception';
 with 'Moose::Exception::Role::Attribute', 'Moose::Exception::Role::Instance';
-    
+
 has 'params' => (
-    is       => 'ro',
-    isa      => 'HashRef',
+    is  => 'ro',
+    isa => 'HashRef'
 );
 
 sub _build_message {
