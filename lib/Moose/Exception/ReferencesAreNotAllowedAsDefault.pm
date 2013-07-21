8e097ba8322239a12388853ced9a7a249ad32659
Upasana Shukla <me@upasana.me>
2013-07-22 03:12:46 +0530

    Wrote ReferencesAreNotAllowedAsDefault & RequiredAttributeLacksInitialization

---
+++ lib/Moose/Exception/ReferencesAreNotAllowedAsDefault.pm
@@ -0,0 +1,25 @@
+package Moose::Exception::ReferencesAreNotAllowedAsDefault;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'attribute_name' => (
+    is        => 'ro',
+    isa       => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "References are not allowed as default values, you must wrap the default of '".
+	$self->attribute_name."' in a CODE reference (ex: sub { [] } and not [])";
+}
+
+1;
