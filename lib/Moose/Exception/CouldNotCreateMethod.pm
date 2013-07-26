68c9444e728efa18d25572ca07a6936a5ddeaa33
Upasana Shukla <me@upasana.me>
2013-07-26 22:51:25 +0530

    Wrote excpetion class CouldNotCreateMethod

---
+++ lib/Moose/Exception/CouldNotCreateMethod.pm
@@ -0,0 +1,30 @@
+package Moose::Exception::CouldNotCreateMethod;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Attribute';
+
+has 'option_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'option_value' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+has 'error' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "Could not create the '".$self->option_name."' method for " . $self->attribute->name . " because : ".$self->error;
+}
+
+1;
