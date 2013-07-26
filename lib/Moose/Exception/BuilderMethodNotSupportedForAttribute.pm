a39604570ff4123ed79f7432538da90f60b619ea
Upasana Shukla <me@upasana.me>
2013-07-26 22:38:55 +0530

    wrote BuilderMethodNotSupportedForAttribute, BadOptionFormat, AttachToClassNeedsAClassMOPClassInstanceOrASubclass & CouldNotCreateMethod

---
+++ lib/Moose/Exception/BuilderMethodNotSupportedForAttribute.pm
@@ -0,0 +1,12 @@
+package Moose::Exception::BuilderMethodNotSupportedForAttribute;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Attribute', 'Moose::Exception::Role::Instance';
+
+sub _build_message {
+    my $self = shift;
+    blessed($self->instance)." does not support builder method '". $self->attribute->builder ."' for attribute '" . $self->attribute->name . "'";
+}
+
+1;
