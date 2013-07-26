ff3000d506f8718bbbb083a63f6873836cf16dee
Upasana Shukla <me@upasana.me>
2013-07-27 03:16:03 +0530

    wrote WrapTakesACodeRefToBless & PackageNameAndNameParamsNotGivenToWrap

---
+++ lib/Moose/Exception/WrapTakesACodeRefToBless.pm
@@ -0,0 +1,24 @@
+package Moose::Exception::WrapTakesACodeRefToBless;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'code' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "You must supply a CODE reference to bless, not (" . $self->code . ")";
+}
+
+1;
