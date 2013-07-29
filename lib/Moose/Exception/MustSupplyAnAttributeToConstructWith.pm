1a6359e27349912439df910209079ec32b20ec8a
Upasana Shukla <me@upasana.me>
2013-07-29 21:26:31 +0530

    Wrote exceptions for Class::MOP::Method::Accessor

---
+++ lib/Moose/Exception/MustSupplyAnAttributeToConstructWith.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::MustSupplyAnAttributeToConstructWith;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    "You must supply an attribute to construct with";
+}
+
+1;
