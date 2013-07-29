9d93e2a143c85d07d73964c62f8795f5e75b19e6
Upasana Shukla <me@upasana.me>
2013-07-29 22:05:28 +0530

    Wrote NoBodyToInitializeInAnAbstractBaseClass & CannotCallAnAbstractBaseMethod

---
+++ lib/Moose/Exception/NoBodyToInitializeInAnAbstractBaseClass.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::NoBodyToInitializeInAnAbstractBaseClass;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'package_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "No body to initialize, " .$self->package_name. " is an abstract base class";
+}
+
+1;
