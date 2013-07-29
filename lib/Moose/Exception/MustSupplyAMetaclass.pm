6df7e99fc0aa3e0236b16612ace5c219c6746082
Upasana Shukla <me@upasana.me>
2013-07-29 21:42:53 +0530

    Wrote exception for MustSupplyAMetaclass

---
+++ lib/Moose/Exception/MustSupplyAMetaclass.pm
@@ -0,0 +1,18 @@
+package Moose::Exception::MustSupplyAMetaclass;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "You must pass a metaclass instance if you want to inline";
+}
+
+1;
