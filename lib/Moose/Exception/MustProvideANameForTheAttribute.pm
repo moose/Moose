43affafb213ba94ee679b57c7cabaa2c42974791
Upasana Shukla <me@upasana.me>
2013-07-31 04:58:49 +0530

    Wrote exceptions for Moose::Meta::Role::Attribute

---
+++ lib/Moose/Exception/MustProvideANameForTheAttribute.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::MustProvideANameForTheAttribute;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    "You must provide a name for the attribute";
+}
+
+1;
