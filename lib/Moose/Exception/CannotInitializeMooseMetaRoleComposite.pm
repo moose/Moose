096c33435d384be3ffb002373406d1936aa831ca
Upasana Shukla <me@upasana.me>
2013-07-31 20:03:30 +0530

    Wrote exceptions for Moose::Meta::Role::Composite

---
+++ lib/Moose/Exception/CannotInitializeMooseMetaRoleComposite.pm
@@ -0,0 +1,28 @@
+package Moose::Exception::CannotInitializeMooseMetaRoleComposite;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'role_composite' => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::Role::Composite',
+    required => 1
+);
+
+has 'old_meta' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+has 'args' => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1    
+);
+
+sub _build_message {
+    'Moose::Meta::Role::Composite instances can only be reinitialized from an existing metaclass instance';
+}
+
+1;
---
+++ lib/Moose/Exception/RolesListMustBeInstancesOfMooseMetaRole.pm
@@ -0,0 +1,24 @@
+package Moose::Exception::RolesListMustBeInstancesOfMooseMetaRole;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'role' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1    
+);
+
+sub _build_message {
+    my $self = shift;
+    "The list of roles must be instances of Moose::Meta::Role, not ".$self->role;
+}
+
+1;
