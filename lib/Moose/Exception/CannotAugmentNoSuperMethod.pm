e07a350f4cef55deb2e861bb67d21d77176006c0
Upasana Shukla <me@upasana.me>
2013-07-31 22:58:45 +0530

    Wrote exception CannotAugmentNoSuperMethod

---
+++ lib/Moose/Exception/CannotAugmentNoSuperMethod.pm
@@ -0,0 +1,24 @@
+package Moose::Exception::CannotAugmentNoSuperMethod;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'method_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "You cannot augment '".$self->method_name."' because it has no super method";
+}
+
+1;
