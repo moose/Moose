f863adf11fa18907b7eedb4b80c52ea358494a98
Upasana Shukla <me@upasana.me>
2013-07-31 22:20:01 +0530

    Wrote exception MustPassAHashOfOptions

---
+++ lib/Moose/Exception/MustPassAHashOfOptions.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::MustPassAHashOfOptions;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    "You must pass a hash of options";
+}
+
+1;
