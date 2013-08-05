b3edbfdb457811c1277ce856e0eb39c78bbc3fad
Upasana Shukla <me@upasana.me>
2013-08-05 23:46:36 +0530

    Wrote exceptions MustHaveAtLeastOneValueToEnumerate & EnumValuesMustBeString

---
+++ lib/Moose/Exception/EnumValuesMustBeString.pm
@@ -0,0 +1,24 @@
+package Moose::Exception::EnumValuesMustBeString;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::ParamsHash';
+
+has 'class' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'value' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1    
+);
+
+sub _build_message {
+    my $self = shift;
+    "Enum values must be strings, not '".( defined $self->value ? $self->value : "undef" )."'";
+}
+
+1;
