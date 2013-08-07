153f7d5a97b0cbd464467e2d3be1ba94b347650a
Upasana Shukla <me@upasana.me>
2013-08-07 23:27:00 +0530

    Wrote exception class InvalidHasProvidedInARole

---
+++ lib/Moose/Exception/InvalidHasProvidedInARole.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::InvalidHasProvidedInARole;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Role';
+
+has 'attribute_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+sub _build_message {
+    "Usage: has 'name' => ( key => value, ... )";
+}
+
+1;
