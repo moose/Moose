7431e351864d0160ea1d126589752c5aad843c9e
Upasana Shukla <me@upasana.me>
2013-08-10 17:37:34 +0530

    Wrote exception CannotCreateMethodAliasLocalMethodIsPresentInClass

---
+++ lib/Moose/Exception/CannotCreateMethodAliasLocalMethodIsPresentInClass.pm
@@ -0,0 +1,17 @@
+package Moose::Exception::CannotCreateMethodAliasLocalMethodIsPresentInClass;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Role', 'Moose::Exception::Role::Method', 'Moose::Exception::Role::Class';
+
+has 'aliased_method_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    "Cannot create a method alias if a local method of the same name exists";
+}
+
+1;
