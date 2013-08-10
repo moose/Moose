8eeacae1646e233be1954a8c39279457babdd159
Upasana Shukla <me@upasana.me>
2013-08-10 17:58:16 +0530

    Added second_role attribute to RoleDoesTheExcludedRole

--- lib/Moose/Exception/RoleDoesTheExcludedRole.pm
+++ lib/Moose/Exception/RoleDoesTheExcludedRole.pm
@@ -16,6 +16,12 @@ has 'second_role' => (
     required => 1
 );
 
+has 'second_role' => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::Role',
+    required => 1
+);
+
 sub _build_message {
     my $self = shift;
     "The role " . $self->role_name . " does the excluded role '".$self->excluded_role->name."'";
