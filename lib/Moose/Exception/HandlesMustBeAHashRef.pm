8989b4cc2a58cdc4349bdeae4edf2113031aa022
Upasana Shukla <me@upasana.me>
2013-08-14 22:31:13 +0530

    Wrote exceptions for Moose::Meta::Attribute::Native::Trait

---
+++ lib/Moose/Exception/HandlesMustBeAHashRef.pm
@@ -0,0 +1,18 @@
+package Moose::Exception::HandlesMustBeAHashRef;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Instance';
+
+has 'given_handles' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    "The 'handles' option must be a HASH reference, not ".$self->given_handles;    
+}
+
+1;
