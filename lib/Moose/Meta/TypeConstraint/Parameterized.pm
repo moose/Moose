7018c3aef27c574757ae281d9ea766ed8af7e156
Upasana Shukla <me@upasana.me>
2013-08-25 00:58:48 +0530

    replaced throw_error with throw_exception

--- lib/Moose/Meta/TypeConstraint/Parameterized.pm
+++ lib/Moose/Meta/TypeConstraint/Parameterized.pm
@@ -7,6 +7,7 @@ use metaclass;
 use Scalar::Util 'blessed';
 use Moose::Util::TypeConstraints;
 use Moose::Meta::TypeConstraint::Parameterizable;
+use Moose::Util 'throw_exception';
 
 use parent 'Moose::Meta::TypeConstraint';
 
@@ -40,15 +41,13 @@ sub compile_type_constraint {
     my $self = shift;
 
     unless ( $self->has_type_parameter ) {
-        require Moose;
-        Moose->throw_error("You cannot create a Higher Order type without a type parameter");
+        throw_exception( CannotCreateHigherOrderTypeWithoutATypeParameter => type => $self );
     }
 
     my $type_parameter = $self->type_parameter;
 
     unless ( blessed $type_parameter && $type_parameter->isa('Moose::Meta::TypeConstraint') ) {
-        require Moose;
-        Moose->throw_error("The type parameter must be a Moose meta type");
+        throw_exception( TypeParameterMustBeMooseMetaType => type => $self );
     }
 
     foreach my $type (Moose::Util::TypeConstraints::get_all_parameterizable_types()) {
@@ -60,9 +59,7 @@ sub compile_type_constraint {
 
     # if we get here, then we couldn't
     # find a way to parameterize this type
-    require Moose;
-    Moose->throw_error("The " . $self->name . " constraint cannot be used, because "
-          . $self->parent->name . " doesn't subtype or coerce from a parameterizable type.");
+    throw_exception( TypeConstraintCannotBeUsedForAParameterizableType => type => $self );
 }
 
 sub can_be_inlined {
