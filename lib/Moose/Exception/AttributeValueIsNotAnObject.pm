860296dcbf1d7a00be1376d1bfe1bc73efa13c03
Upasana Shukla <me@upasana.me>
2013-08-25 01:57:42 +0530

    Wrote exception classes AttributeValueIsNot(Defined|AnObject)

---
+++ lib/Moose/Exception/AttributeValueIsNotAnObject.pm
@@ -0,0 +1,26 @@
+package Moose::Exception::AttributeValueIsNotAnObject;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Instance', 'Moose::Exception::Role::Attribute';
+
+has 'method' => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::Method::Delegation',
+    required => 1,
+);
+
+has 'given_value' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    "Cannot delegate ".$self->method->name." to "
+    .$self->method->delegate_to_method." because the value of "
+    . $self->attribute->name . " is not an object (got '".$self->given_value."')";
+}
+
+1;
