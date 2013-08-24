6622604557a4701718bc6bfea76eb2839d5460bd
Upasana Shukla <me@upasana.me>
2013-08-25 00:42:46 +0530

    replaced _inline_throw_error with _inline_throw_exception

--- lib/Moose/Meta/Method/Accessor/Native/String/match.pm
+++ lib/Moose/Meta/Method/Accessor/Native/String/match.pm
@@ -19,9 +19,11 @@ sub _inline_check_arguments {
 
     return (
         'if (!Moose::Util::_STRINGLIKE0($_[0]) && !Params::Util::_REGEX($_[0])) {',
-            $self->_inline_throw_error(
-                '"The argument passed to match must be a string or regexp '
-              . 'reference"',
+            $self->_inline_throw_exception( "InvalidArgumentToMethod => ".
+                                            'argument                => $_[0],'.
+                                            'type                    => "Str|RegexpRef",'.
+                                            'type_of_argument        => "string or regexp reference",'.
+                                            'method_name             => "match"',
             ) . ';',
         '}',
     );
