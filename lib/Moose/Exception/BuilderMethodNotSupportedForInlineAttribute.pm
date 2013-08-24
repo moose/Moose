9d3502eb9bcaa13b49e9546a78ff925e3a90589a
Upasana Shukla <me@upasana.me>
2013-08-25 03:22:40 +0530

    Wrote exception classes for Moose::Meta::Attribute

---
+++ lib/Moose/Exception/BuilderMethodNotSupportedForInlineAttribute.pm
@@ -0,0 +1,24 @@
+package Moose::Exception::BuilderMethodNotSupportedForInlineAttribute;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Instance', 'Moose::Exception::Role::Class';
+
+has 'attribute_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'builder' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    $self->class_name." does not support builder method '". $self->builder ."' for attribute '" . $self->attribute_name . "'";
+}
+
+1;
