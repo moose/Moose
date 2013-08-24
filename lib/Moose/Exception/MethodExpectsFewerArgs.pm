ce04d86e8191af16e5abdb96af93833c293344b6
Upasana Shukla <me@upasana.me>
2013-08-25 00:46:48 +0530

    Wrote exception classes for Moose::Meta::Method::Accessor::Native

---
+++ lib/Moose/Exception/MethodExpectsFewerArgs.pm
@@ -0,0 +1,25 @@
+package Moose::Exception::MethodExpectsFewerArgs;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'method_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has 'maximum_args' => (
+    is       => 'ro',
+    isa      => 'Int',
+    required => 1,
+);
+
+sub _build_message {
+    my $self = shift;
+    my $max = $self->maximum_args;
+    "Cannot call ".$self->method_name." with ".
+        ( $max ? "more than $max" : 'any'). " argument".( $max == 1 ? '' : 's' );
+}
+
+1;
