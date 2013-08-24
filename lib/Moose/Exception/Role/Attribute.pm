6a54e90360faa8774aa7324143eb54d6db9feda5
Upasana Shukla <me@upasana.me>
2013-08-24 22:30:39 +0530

    added predicate to 'attribute'

--- lib/Moose/Exception/Role/Attribute.pm
+++ lib/Moose/Exception/Role/Attribute.pm
@@ -3,9 +3,9 @@ package Moose::Exception::Role::Attribute;
 use Moose::Role;
 
 has 'attribute' => (
-    is       => 'ro',
-    isa      => 'Class::MOP::Attribute',
-    required => 1,
+    is        => 'ro',
+    isa       => 'Class::MOP::Attribute',
+    predicate => 'is_attribute_set'
 );
 
 1;
