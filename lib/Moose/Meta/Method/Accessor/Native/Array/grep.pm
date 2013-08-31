e0d06cc2730c2329e524d1c2f7ec31546a776bd4
Upasana Shukla <me@upasana.me>
2013-09-01 04:42:13 +0530

    replaced _inline_throw_error with _inline_throw_exception

--- lib/Moose/Meta/Method/Accessor/Native/Array/grep.pm
+++ lib/Moose/Meta/Method/Accessor/Native/Array/grep.pm
@@ -18,8 +18,11 @@ sub _inline_check_arguments {
 
     return (
         'if (!Params::Util::_CODELIKE($_[0])) {',
-            $self->_inline_throw_error(
-                '"The argument passed to grep must be a code reference"',
+            $self->_inline_throw_exception( "InvalidArgumentToMethod => ".
+                                            'argument                => $_[0],'.
+                                            'method_name             => "grep",'.
+                                            'type_of_argument        => "code reference",'.
+                                            'type                    => "CodeRef",',
             ) . ';',
         '}',
     );
