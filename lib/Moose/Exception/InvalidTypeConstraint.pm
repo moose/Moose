0fe01796d595355ca00bed725cb6ae7686176b59
Upasana Shukla <me@upasana.me>
2013-09-15 01:39:47 +0530

    Wrote exception for Moose::Meta::TypeConstraints::Registry

---
+++ lib/Moose/Exception/InvalidTypeConstraint.pm
@@ -0,0 +1,22 @@
+package Moose::Exception::InvalidTypeConstraint;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'registry_object' => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::TypeConstraint::Registry',
+    required => 1
+);
+
+has 'type' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+sub _build_message {
+    return "No type supplied / type is not a valid type constraint";
+}
+
+1;
