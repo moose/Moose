0085e834a60e93f672a89be37b748b7eb282d978
Upasana Shukla <me@upasana.me>
2013-09-15 01:33:14 +0530

    Wrote exceptions for Moose::Meta::TypeCoercion::Union

---
+++ lib/Moose/Exception/NeedsTypeConstraintUnionForTypeCoercionUnion.pm
@@ -0,0 +1,21 @@
+package Moose::Exception::NeedsTypeConstraintUnionForTypeCoercionUnion;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::TypeConstraint';
+
+has 'type_coercion_union_object' => (
+    is       => 'ro',
+    isa      => 'Moose::Meta::TypeCoercion::Union',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    my $type_constraint = $self->type;
+
+    return "You can only create a Moose::Meta::TypeCoercion::Union for a " .
+           "Moose::Meta::TypeConstraint::Union, not a $type_constraint"    
+}
+
+1;
