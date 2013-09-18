2caab5f4b733bb18f7a0857b03106007f94659a1
Upasana Shukla <me@upasana.me>
2013-09-18 08:02:48 +0530

    wrote CouldNotParseType

---
+++ lib/Moose/Exception/CouldNotParseType.pm
@@ -0,0 +1,28 @@
+package Moose::Exception::CouldNotParseType;
+
+use Moose;
+extends 'Moose::Exception';
+
+has 'type' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has 'position' => (
+    is       => 'ro',
+    isa      => 'Int',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    my $type = $self->type;
+    my $length = length($type);
+    my $position = $self->position;
+
+    return "'$type' didn't parse (parse-pos=$position"
+        . " and str-length=$length)";
+}
+
+1;
