531ee9a6f71cdabd7668ad2d25be3395f753b8b2
Upasana Shukla <me@upasana.me>
2013-09-18 07:58:11 +0530

    Wrote exceptions for Moose::Exporter

---
+++ lib/Moose/Exception/InvalidArgumentsToTraitAliases.pm
@@ -0,0 +1,30 @@
+package Moose::Exception::InvalidArgumentsToTraitAliases;
+
+use Moose;
+extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class';
+
+use Scalar::Util qw(reftype);
+
+has 'alias' => (
+    is       => 'ro',
+    isa      => 'Any',
+    required => 1
+);
+
+has 'package_name' => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+sub _build_message {
+    my $self = shift;
+    my $alias = $self->alias;
+    my $reftype_alias = reftype($alias);
+
+    return "$reftype_alias references are not "
+        . "valid arguments to the 'trait_aliases' option";
+}
+
+1;
