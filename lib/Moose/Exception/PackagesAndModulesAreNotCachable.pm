4835932d12b84f4ebd4a95f26aaa2a9820bbda94
Upasana Shukla <me@upasana.me>
2013-09-22 01:48:54 +0530

    Renamed package_or_module to is_module & made it Bool

--- lib/Moose/Exception/PackagesAndModulesAreNotCachable.pm
+++ lib/Moose/Exception/PackagesAndModulesAreNotCachable.pm
@@ -4,16 +4,21 @@ use Moose;
 extends 'Moose::Exception';
 with 'Moose::Exception::Role::Class', 'Moose::Exception::Role::ParamsHash';
 
-has 'package_or_module' => (
+has 'is_module' => (
     is       => 'ro',
-    isa      => 'Str',
+    isa      => 'Bool',
     required => 1
 );
 
 sub _build_message {
     my $self = shift;
-    my $package_or_module = $self->package_or_module;
-    return "$package_or_module are not cacheable";
+    my $is_module = $self->is_module;
+
+    if( $is_module ) {
+        return "Modules are not cacheable";
+    } else {
+        return "Packages are not cacheable";
+    }
 }
 
 1;
