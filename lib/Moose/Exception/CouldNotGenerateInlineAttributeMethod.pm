688ef4db28bee13d38058fef405a91b1b2f775c6
Upasana Shukla <me@upasana.me>
2013-09-23 07:57:14 +0530

    Changed type of error, error isa Str|Moose::Exception
    This exception can have CouldNotCreateWriter as an error, so error should be 'Str|Moose::Exception'

--- lib/Moose/Exception/CouldNotGenerateInlineAttributeMethod.pm
+++ lib/Moose/Exception/CouldNotGenerateInlineAttributeMethod.pm
@@ -4,12 +4,17 @@ use Moose;
 extends 'Moose::Exception';
 with 'Moose::Exception::Role::Instance';
 
-has [qw(error option)] => (
+has 'option' => (
     is       => 'ro',
     isa      => 'Str',
     required => 1
 );
 
+has 'error' => (
+    is       => 'ro',
+    isa      => 'Str|Moose::Exception',
+    required => 1
+);
 
 sub _build_message {
     my $self = shift;
