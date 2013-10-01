37393ec8cce38a444de65cb761792a1211a14da5
Upasana Shukla <me@upasana.me>
2013-10-01 14:47:54 +0530

    Added one more attribute(class) to ExtendsMissingArgs

--- lib/Moose/Exception/ExtendsMissingArgs.pm
+++ lib/Moose/Exception/ExtendsMissingArgs.pm
@@ -2,6 +2,7 @@ package Moose::Exception::ExtendsMissingArgs;
 
 use Moose;
 extends 'Moose::Exception';
+with 'Moose::Exception::Role::Class';
 
 sub _build_message {
     "Must derive at least one class";
