71cf8e8cbc506eb1d26d2227f5c1b06a582acb96
Karen Etheridge <ether@cpan.org>
2013-10-19 11:08:05 -0700

    economize on bytes - only set when needed

--- inc/TestRelease.pm
+++ inc/TestRelease.pm
@@ -8,12 +8,8 @@ around before_release => sub {
     my $orig = shift;
     my $self = shift;
 
-    local $ENV{MOOSE_TEST_MD} = $self->zilla->is_trial
-        ? $ENV{MOOSE_TEST_MD}
-        : 1;
-    local $ENV{AUTHOR_TESTING} = $self->zilla->is_trial
-        ? $ENV{AUTHOR_TESTING}
-        : 1;
+    local $ENV{MOOSE_TEST_MD} = 1 if not $self->zilla->is_trial;
+    local $ENV{AUTHOR_TESTING} = 1 if not $self->zilla->is_trial;
 
     $self->$orig(@_);
 };
