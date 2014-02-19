e15ef14ac2a0c12301f2c0dc676d5868d3418f2e
Upasana <me@upasana.me>
2014-02-20 03:08:45 +0530

    No need of type or type_name for this exception
    If anyone gets this exception, then the problem is in
    the user's code & it needs to be fixed.

--- lib/Moose/Exception/CannotRegisterUnnamedTypeConstraint.pm
+++ lib/Moose/Exception/CannotRegisterUnnamedTypeConstraint.pm
@@ -3,12 +3,6 @@ package Moose::Exception::CannotRegisterUnnamedTypeConstraint;
 use Moose;
 extends 'Moose::Exception';
 
-has 'type' => (
-    is       => 'ro',
-    isa      => 'Moose::Meta::TypeConstraint',
-    required => 1,
-);
-
 sub _build_message {
     "can't register an unnamed type constraint";
 }
