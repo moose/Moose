e2c4443fe25e50852a4f4eaffbf7ac2f93d39b1c
Karen Etheridge <ether@cpan.org>
2014-08-22 17:18:53 -0700

    add comment to my ugly hack

--- inc/SimpleProvides.pm
+++ inc/SimpleProvides.pm
@@ -18,6 +18,9 @@ sub metadata
     return +{
         provides => {
             map {
+                # this is an awful hack and assumes ascii package names:
+                # please do not cargo-cult this code elsewhere. The proper
+                # thing to do is to crack open the file and read the pod name.
                 my $filename = $_->name;
                 (my $package = $filename) =~ s{[/\\]}{::}g;
                 $package =~ s/^lib:://;
