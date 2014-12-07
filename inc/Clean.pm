21ef755e4df75e807853bff462083f71a55c81ed
Karen Etheridge <ether@cpan.org>
2014-12-06 20:51:46 -0800

    keep unwanted files out of the shipped dist
    - run 'make clean' after the build finishes, too -- cleans up build artifacts
      left behind from generating docs so they don't get shipped
    - wait for 'make clean' to finish before letting [GatherDir] grab everything
      (don't you love debugging race conditions? I sure do!)
    - compare the built dist dir against our expected list of files to ensure that
      we didn't neglect to clean anything

--- inc/Clean.pm
+++ inc/Clean.pm
@@ -1,19 +1,46 @@
 package inc::Clean;
 use Moose;
 
-with 'Dist::Zilla::Role::BeforeBuild';
+with 'Dist::Zilla::Role::BeforeBuild',
+    'Dist::Zilla::Role::AfterBuild';
+use Path::Tiny;
+use File::pushd 'pushd';
 
-sub before_build {
-    my $self = shift;
+sub before_build { shift->_clean('.') }
 
+sub after_build {
+    my ($self, $opts) = @_;
+
+    $self->_clean($opts->{build_root});
+
+    my $iter = path($opts->{build_root})->iterator({ recurse => 1 });
+    my %found_files;
+    while (my $found_file = $iter->()) {
+        next if -d $found_file;
+        ++$found_files{ $found_file->relative($opts->{build_root}) };
+    }
+    delete $found_files{$_->name} foreach @{ $self->zilla->files };
+
+    $self->log(join("\n",
+        "WARNING: Files were left behind in $opts->{build_root} that were not explicitly added:",
+        sort keys %found_files,
+    )) if keys %found_files;
+}
+
+sub _clean {
+    my ($self, $build_dir) = @_;
+
+    my $cwd = pushd $build_dir;
     if (-e 'Makefile') {
-        $self->log("Running make distclean to clear out build cruft");
-        unless (fork) {
+        $self->log("Running make distclean in $build_dir to clear out build cruft");
+        my $pid = fork;
+        unless ($pid) {
             close(STDIN);
             close(STDOUT);
             close(STDERR);
             { exec("$^X Makefile.PL && make distclean") }
             die "couldn't exec: $!";
         }
+        waitpid($pid, 0) if $pid;
     }
 }
