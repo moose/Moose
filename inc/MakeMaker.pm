8ec1e6ba50ed843c3816edaad3e66fe5014c492c
Karen Etheridge <ether@cpan.org>
2016-04-21 15:05:55 +0100

    perform compiler check with ExtUtils::HasCompiler

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -13,50 +13,11 @@ override _build_MakeFile_PL_template => sub {
 
     my $tmpl = super();
     my $assert_compiler = <<'ASSERT_COMPILER';
-# Secondary compile testing via ExtUtils::CBuilder
-sub can_xs {
-    # Do we have the configure_requires checker?
-    unless (eval 'require ExtUtils::CBuilder; ExtUtils::CBuilder->VERSION(0.27); 1') {
-        # They don't obey configure_requires, so it is
-        # someone old and delicate. Try to avoid hurting
-        # them by falling back to an older simpler test.
-        return can_cc();
-    }
-
-    return ExtUtils::CBuilder->new( quiet => 1 )->have_compiler;
-}
-
-# can we locate a (the) C compiler
-sub can_cc {
-  my @chunks = split(/ /, $Config::Config{cc}) or return;
-
-  # $Config{cc} may contain args; try to find out the program part
-  while (@chunks) {
-    return can_run("@chunks") || (pop(@chunks), next);
-  }
-
-  return;
-}
-
-# check if we can run some command
-sub can_run {
-  my ($cmd) = @_;
-
-  return $cmd if -x $cmd;
-  if (my $found_cmd = MM->maybe_command($cmd)) {
-    return $found_cmd;
-  }
-
-  for my $dir ((split /$Config::Config{path_sep}/, $ENV{PATH}), '.') {
-    next if $dir eq '';
-    my $abs = File::Spec->catfile($dir, $cmd);
-    return $abs if (-x $abs or $abs = MM->maybe_command($abs));
-  }
-
-  return;
-}
-
-die 'This distribution requires a working compiler' unless can_xs();
+# Secondary compile testing via ExtUtils::HasCompiler
+use lib 'inc';
+use ExtUtils::HasCompiler 0.013 'can_compile_loadable_object';
+die 'This distribution requires a working compiler'
+    unless can_compile_loadable_object(quiet => 1);
 
 ASSERT_COMPILER
 
