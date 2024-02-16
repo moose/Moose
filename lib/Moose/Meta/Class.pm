49becf419eb2b956c0869cb2e5d65530e26f12b0
Daniel Böhmer <post@daniel-boehmer.de>
2024-02-16 18:42:33 +0100

    Fix term "array" into "list" 2x in POD for Moose::Meta::Class

--- lib/Moose/Meta/Class.pm
+++ lib/Moose/Meta/Class.pm
@@ -892,12 +892,12 @@ This adds an C<augment> method modifier to the package.
 
 =item B<< $metaclass->calculate_all_roles >>
 
-This will return a unique array of L<Moose::Meta::Role> instances
+This will return a unique list of L<Moose::Meta::Role> instances
 which are attached to this class.
 
 =item B<< $metaclass->calculate_all_roles_with_inheritance >>
 
-This will return a unique array of L<Moose::Meta::Role> instances
+This will return a unique list of L<Moose::Meta::Role> instances
 which are attached to this class, and each of this class's ancestors.
 
 =item B<< $metaclass->add_role($role) >>
